391af16346dedd093fba5f54490c17def9d8d531
falahyan <iyanflx24@gmail.com>
2026-06-25 14:50:14 +0700

    fix: missing key file

--- script/employee_jabatan/importer.js
+++ script/employee_jabatan/importer.js
@@ -26,8 +26,8 @@ const BKN_DOC_ID_TO_FILE_KEY = {
 
 // --- This is your mapping from 'fileKey' to DB column info ---
 const LOCAL_FILE_KEY_MAPPING = {
-  skJabatan: { fileType: 11, field: "trx_jabatan_file_id" },
-  spPelantikan: { fileType: 40, field: "trx_jabatan_file_spp" },
+  SK_JABATAN: { fileType: 11, field: "trx_jabatan_file_id" },
+  SK_PELANTIKAN: { fileType: 40, field: "trx_jabatan_file_spp" },
   baJabatan: { fileType: 41, field: "trx_jabatan_file_ba" },
 };
 
